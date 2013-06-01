f9718022430a9b7c8de3dafd2275f94a1248df15
Bruno Henrique Stein <brunohstein@gmail.com>
2013-06-01 19:24:17 -0300

    adiciona fontes e botões

--- config/router.js
+++ config/router.js
@@ -8,22 +8,22 @@ router.get('/login').to('main.login');
 router.get('/logout').to('main.logout');
 router.post('/auth/local').to('auth.local');
 
-router.get('/users(.:format)').to('users.index');
-router.get('/users/add(.:format)').to('users.add');
-router.get('/:user(.:format)').to('users.show');
-router.get('/:user/edit(.:format)').to('users.edit');
-router.post('/users(.:format)').to('users.create');
-router.put('/:user(.:format)').to('users.update');
-router.del('/:user(.:format)').to('rooms.destroy');
+router.get('/users').to('users.index');
+router.get('/users/add').to('users.add');
+router.get('/:user').to('users.show');
+router.get('/:user/edit').to('users.edit');
+router.post('/users').to('users.create');
+router.put('/:user').to('users.update');
+router.del('/:user').to('rooms.destroy');
 
-router.get('/:user/rooms(.:format)').to('rooms.index');
-router.get('/:user/rooms/add(.:format)').to('rooms.add');
-router.get('/:user/:room(.:format)').to('rooms.show');
-router.get('/:user/:room/edit(.:format)').to('rooms.edit');
-router.post('/:user/rooms(.:format)').to('rooms.create');
-router.put('/:user/:room(.:format)').to('rooms.update');
-router.del('/:user/:room(.:format)').to('rooms.destroy');
+router.get('/:user/rooms').to('rooms.index');
+router.get('/:user/rooms/add').to('rooms.add');
+router.get('/:user/:room').to('rooms.show');
+router.get('/:user/:room/edit').to('rooms.edit');
+router.post('/:user/rooms').to('rooms.create');
+router.put('/:user/:room').to('rooms.update');
+router.del('/:user/:room').to('rooms.destroy');
 
-router.post('/:user/:room(.:format)').to('messages.create');
+router.post('/:user/:room').to('messages.create');
 
 exports.router = router;
